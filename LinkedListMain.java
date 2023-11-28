c968f13ad683c298654c7f956f2987d77c1b2029
Vishnupriya <priya.singh9349@gmail.com>
2023-11-28 22:46:32 +0530

    Added code for UC4

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -2,9 +2,10 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
-        list.append(56); 
-        list.append(30); 
-        list.append(70); 
+        list.append(56);
+        list.append(70);
+
+        list.insertAfter(56, 30);
 
         list.printList(); 
     }
