d94b0fda5c9b8cc00975b0a6002d430bce28de55
Vishnupriya <priya.singh9349@gmail.com>
2023-11-28 22:27:28 +0530

    Added code for UC1

---
+++ LinkedList.java
@@ -0,0 +1,26 @@
+public class LinkedList {
+    Node head;
+
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = new Node(data);
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
