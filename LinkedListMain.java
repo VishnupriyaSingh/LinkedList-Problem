ea7fcdf8e4a1194b29ecbb58ecc87bda1b3f1585
Vishnupriya <priya.singh9349@gmail.com>
2023-11-28 22:55:33 +0530

    Added code for UC9

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,10 +3,11 @@ public class LinkedListMain {
         LinkedList list = new LinkedList();
         list.append(56);
         list.append(30);
+        list.append(40);
         list.append(70);
         list.printList();
 
-        list.insertAfterValue(30, 40);
+        list.deleteByValue(40);
 
         list.printList();
     }
