f7e8a1acaf326b48fdb1ffd5fe6aa8e51956218a
Vishnupriya <priya.singh9349@gmail.com>
2023-11-28 22:32:03 +0530

    Added code for UC2

--- LinkedList.java
+++ LinkedList.java
@@ -1,18 +1,10 @@
 public class LinkedList {
     Node head;
 
-    public void append(int data) {
-        if (head == null) {
-            head = new Node(data);
-            return;
-        }
-
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-
-        current.next = new Node(data);
+    public void addAtStart(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
     }
 
     public void printList() {
--- LinkedListMain.java
+++ LinkedListMain.java
@@ -2,10 +2,10 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
-        list.append(56);
-        list.append(30);
-        list.append(70);
+        list.addAtStart(70); 
+        list.addAtStart(30); 
+        list.addAtStart(56); 
 
-        list.printList();
+        list.printList(); 
     }
 }
