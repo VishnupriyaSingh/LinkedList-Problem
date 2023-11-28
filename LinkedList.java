2cd893ba8336bc71cee85c3df3300b8ceb3cbc09
Vishnupriya <priya.singh9349@gmail.com>
2023-11-28 22:44:27 +0530

    Added code for UC3

--- LinkedList.java
+++ LinkedList.java
@@ -1,10 +1,18 @@
 public class LinkedList {
     Node head;
 
-    public void addAtStart(int data) {
-        Node newNode = new Node(data);
-        newNode.next = head;
-        head = newNode;
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = new Node(data);
     }
 
     public void printList() {
